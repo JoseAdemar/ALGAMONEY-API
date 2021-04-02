56a157297baebfd0000e99c6af3f10fd197acb5f
josejunior <joseademarti@gmail.com>
2021-04-02 17:40:10 -0300

    Criada a validação dos campos com Bean Validation e HandlerException

--- src/main/java/com/example/algamoney/api/model/Categoria.java
+++ src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,5 +21,7 @@ public class Categoria {
 	 @EqualsAndHashCode.Include
 	 Long codigo;
 	 
+	 @NotNull
+	 @Size(min = 3, max = 20)
 	 private String nome;
 }
