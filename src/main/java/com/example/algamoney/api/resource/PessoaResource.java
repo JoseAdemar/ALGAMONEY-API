b2dd81b6efb89bdab9ac5b897ac66ab9ed419368
josejunior <joseademarti@gmail.com>
2021-04-06 16:45:17 -0300

    Criado o metodo de atualizar da classe Pessoa

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,5 +1,7 @@
 package com.example.algamoney.api.resource;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -7,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
+import com.example.algamoney.api.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -27,6 +32,9 @@ public class PessoaResource {
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
+	
+	@Autowired
+	private PessoaService pessoaService;
 	
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
@@ -43,5 +51,17 @@ public class PessoaResource {
 		Pessoa pessoa = pessoaRepository.findById(codigo).get();
 		 return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
 	}
-	
+ 	
+ 	@DeleteMapping("/{codigo}")
+ 	public ResponseEntity remover(@PathVariable Long codigo) {
+ 		
+ 		pessoaRepository.deleteById(codigo);
+ 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+ 	}
+ 	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar
+	(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+		Optional<Pessoa> pessoaSalva = Optional.of(pessoaService.atualizar(codigo, pessoa));
+		return ResponseEntity.status(HttpStatus.OK).body(pessoa);
+	}
 }
