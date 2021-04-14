c272bdf99258dd2d751daba0bd2ec8a34e1d8f25
Jose Ademar De Oliveira Junior <jose.oliveira@palmares.gov.br>
2021-04-14 15:15:46 -0300

    Implementado a paginação em lançamento

--- src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
@@ -11,53 +11,77 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
 
-
-
-
 public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 
 	@PersistenceContext
 	private EntityManager manager;
-	
+
 	@Override
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
 		Root<Lancamento> root = criteria.from(Lancamento.class);
-		
+
 		Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
 		criteria.where(predicates);
-		
+
 		TypedQuery<Lancamento> query = manager.createQuery(criteria);
-		return query.getResultList();
+
+		adicionarRestricoesDePaginacao(query, pageable);
+		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
 	}
 
 	@SuppressWarnings("deprecation")
 	private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
 			Root<Lancamento> root) {
 		List<Predicate> predicates = new ArrayList<>();
-		
+
 		if (!StringUtils.isEmpty(lancamentoFilter.getDescricao())) {
-			predicates.add(builder.like(
-					builder.lower(root.get("descricao")), "%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
+			predicates.add(builder.like(builder.lower(root.get("descricao")),
+					"%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
 		}
-		
+
 		if (lancamentoFilter.getDataVencimentoDe() != null) {
 			predicates.add(
 					builder.greaterThanOrEqualTo(root.get("dataVencimento"), lancamentoFilter.getDataVencimentoDe()));
 		}
-		
+
 		if (lancamentoFilter.getDataVencimentoAte() != null) {
 			predicates.add(
 					builder.lessThanOrEqualTo(root.get("dataVencimento"), lancamentoFilter.getDataVencimentoAte()));
 		}
-		
+
 		return predicates.toArray(new Predicate[predicates.size()]);
+	}
+
+	private Long total(LancamentoFilter lancamentoFilter) {
+
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+		Root<Lancamento> root = criteria.from(Lancamento.class);
+
+		Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
+
+		criteria.select(builder.count(root));
+		return manager.createQuery(criteria).getSingleResult();
+
+	}
+
+	private void adicionarRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+		int paginaAtual = pageable.getPageNumber();
+		int totalRegistrosPorPagina = pageable.getPageSize();
+		int primeiroRegistroDaPag = paginaAtual * totalRegistrosPorPagina;
+
+		query.setFirstResult(primeiroRegistroDaPag);
+		query.setMaxResults(totalRegistrosPorPagina);
 	}
 
 }
--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -2,6 +2,7 @@ package com.example.algamoney.api.resource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -10,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,83 +36,86 @@ import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaExc
 @RequestMapping("/lancamentos")
 public class LancamentoResource {
 
-	   @Autowired
-	   private LancamentoRepository lancamentoRepository;
-	   
-	   @Autowired
-	   LancamentoService lancamentoService;
-	   
-	   @Autowired
-	   private ApplicationEventPublisher publisher;
-	   
-	   @Autowired
-	   private MessageSource messageSource;
-	   
-	   
-	   @GetMapping
-	   public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter){
-		   
-		      return  lancamentoRepository.filtrar(lancamentoFilter);
-	   }
-	   
-	   
-	  @GetMapping("/{codigo}")
-	  public ResponseEntity<Lancamento>lancamento(@PathVariable Long codigo) {
-		  
-		  try {
-		   Lancamento buscarLancamento = lancamentoRepository.findById(codigo).get();
-		   
-		    if(buscarLancamento != null) {
-			   
-			  return ResponseEntity.ok(buscarLancamento);
-			  
-		   }
-		   }catch (Exception e) {
-			
-			   return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		   }
-		   
-		   return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		   
-		  
-	      }
-	  
-	     @PostMapping()
-	     public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
-	    	 
-	    	 Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
-	    	    try {
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
 
-	    	    publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
-	    	    
-	    	    return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
-	    	    
-	    	    }catch (Exception e) {
-					
-	    	    	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(lancamentoSalvo);
-				}
-	    	    
-	    	 
-	     }
-	     
-	     @ExceptionHandler({ PessoaInexistenteOuInativaException.class })
-	 	    public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
-	 		String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
-	 		String mensagemDesenvolvedor = ex.toString();
-	 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
-	 		return ResponseEntity.badRequest().body(erros);
-	 	}
+	@Autowired
+	LancamentoService lancamentoService;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@GetMapping
+	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
+	}
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Lancamento> lancamento(@PathVariable Long codigo) {
+
+		try {
+			Lancamento buscarLancamento = lancamentoRepository.findById(codigo).get();
+
+			if (buscarLancamento != null) {
+
+				return ResponseEntity.ok(buscarLancamento);
+
+			}
+		} catch (Exception e) {
+
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+	}
+
+	@PostMapping()
+	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+
+		Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
+		try {
+
+			publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+
+			return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+
+		} catch (Exception e) {
+
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(lancamentoSalvo);
+		}
+
+	}
+
+	@DeleteMapping("/{codigo}")
+	public ResponseEntity<Lancamento> remover(@PathVariable Long codigo) {
+
+		try {
+			Optional<Lancamento> lancamento = lancamentoRepository.findById(codigo);
+
+			if (lancamento.isPresent()) {
+
+				lancamentoRepository.delete(lancamento.get());
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).body(lancamento.get());
+			}
+		} catch (Exception e) {
+
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+	}
+
+	@ExceptionHandler({ PessoaInexistenteOuInativaException.class })
+	public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
+		String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null,
+				LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return ResponseEntity.badRequest().body(erros);
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
